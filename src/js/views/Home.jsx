3b94913d0d9d9a528d180a66f3e87be54a3f4748
Carlos Atanes Vences <carlos117g@gmail.com>
2024-02-21 13:36:33 +0000

    fix: images on home.jsx
    Se han añadido imagenes al carrousel, no estan muy ajustadas pero es lo que hay

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -2,6 +2,10 @@ import React, { useContext, useState } from "react";
 import { Carousel } from 'react-bootstrap';
 import { useNavigate } from "react-router";
 import { Context } from "../store/appContext";
+import swChaImg from "../../img/swcharacter.jpg";
+import swPlaImg from "../../img/swplanets.jpg";
+import ConListImg from "../../img/contactlist.jpeg";
+
 
 export const Home = () => {
     const { store, actions } = useContext(Context)
@@ -19,23 +23,23 @@ export const Home = () => {
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
-                <img src='https://placehold.co/800x200' onClick={handleClick} className='d-block w-100' />
-                <Carousel.Caption className="text-dark">
+                <img src={ConListImg} onClick={handleClick} className='carrouselImg d-block w-100' />
+                <Carousel.Caption className="text-light">
                     <h3>Contact List</h3>
                     <p>Trabajo de <a href="https://4geeks.com/syllabus/spain-fs-pt-54/project/contact-list-context">4GeeksAcademy "Contact List"</a>.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img src='https://placehold.co/800x200' onClick={handleClick} className='d-block w-100' />
-                <Carousel.Caption className="text-dark">
+                <img src={swChaImg} onClick={handleClick} className='carrouselImg d-block w-100' />
+                <Carousel.Caption className="text-light">
                     <h3>Personajes</h3>
                     <p>Información sobre los personajes de StarWars.</p>
                     <p>Parte del trabajo de <a href="https://4geeks.com/syllabus/spain-fs-pt-54/project/starwars-blog-reading-list">4GeeksAcademy "Star Wars"</a></p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img src='https://placehold.co/800x200' onClick={handleClick} className='d-block w-100' />
-                <Carousel.Caption className="text-dark">
+                <img src={swPlaImg} onClick={handleClick} className='carrouselImg d-block w-100' />
+                <Carousel.Caption className="text-light">
                     <h3>Planetas</h3>
                     <p>Información sobre los planetas de StarWars.</p>
                     <p>Parte del trabajo de <a href="https://4geeks.com/syllabus/spain-fs-pt-54/project/starwars-blog-reading-list">4GeeksAcademy "Star Wars"</a></p>
